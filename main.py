e99414e9e0b9a623eadd23ff0272347530fbada2
khisan <shinaruikhisan@gmail.com>
2025-05-27 00:12:26 +0700

    generate eth wallet

--- main.py
+++ main.py
@@ -1,9 +1,68 @@
 from utils.eth_wallet import eth_wallet_generator
 import os
+from colorama import Fore, Style
+from rich.console import Console
+from rich.panel import Panel
 
-os.system('cls' if os.name == 'nt' else 'clear')
 
-generator = eth_wallet_generator()
-wallets = generator.generate_batch_eth_wallets(num_wallets=1)
-generator.export_wallets_to_csv(wallets, filename="1_eth_wallets")
-generator.export_wallets_to_excel(wallets, filename="1_eth_wallets")
+if __name__ == "__main__":
+
+    def print_banner():
+        os.system('cls' if os.name == 'nt' else 'clear')
+        console = Console()
+        banner = """[bold green]                                                          
+                    █░█░█ ▄▀█ █░░ █░░ █▀▀ ▀█▀   █▀▀ █▀▀ █▄░█ █▀▀ █▀█ ▄▀█ ▀█▀ █▀█ █▀█
+                    ▀▄▀▄▀ █▀█ █▄▄ █▄▄ ██▄ ░█░   █▄█ ██▄ █░▀█ ██▄ █▀▄ █▀█ ░█░ █▄█ █▀▄                                                                                                                                 
+        [/bold green] """
+        console.print(Panel(banner, title="[bold yellow]Simple Wallet Generator[/bold yellow]",
+                            title_align="center",
+                            subtitle="By Meowstronot (Khisan)", subtitle_align="center", 
+                            border_style="cyan", width=100))
+
+    def main_menu():
+
+        print_banner()
+        menu = ["Generate Ethereum Wallets",
+                "Exit"]
+
+        print(Fore.LIGHTBLUE_EX + Style.BRIGHT + "Main Menu :" + Style.RESET_ALL)
+        for i, option in enumerate(menu, start=1):
+            print(f"{Fore.GREEN + Style.BRIGHT}{i:>6}.{Style.RESET_ALL} {option}")
+        
+        while True:
+            try:
+                input_chain = int(input(f"Select an option {Fore.GREEN + Style.BRIGHT}[1 to {len(menu)}]{Style.RESET_ALL}: "))
+                if 1 <= input_chain <= len(menu):
+                    if input_chain == len(menu):
+                        print(Fore.YELLOW + "Exiting the program..." + Style.RESET_ALL)
+                        exit()
+                    break
+                else:
+                    print(Fore.RED + f"Please enter a number [1 to {len(menu)}]" + Style.RESET_ALL)
+            except ValueError:
+                print(Fore.RED + f"Invalid input. Please enter a number [1 to {len(menu)}]" + Style.RESET_ALL)
+        
+        while True: 
+            try: 
+                num_wallets = int(input("Enter the number of wallets to generate : "))
+                if num_wallets > 0:
+                    break
+                else:
+                    print(Fore.RED + "Please enter a positive number." + Style.RESET_ALL)
+            except ValueError:
+                print(Fore.RED + "Invalid input. Please enter a valid number." + Style.RESET_ALL)
+
+        result_print = (f"You selected to generate {Fore.GREEN + Style.BRIGHT}{num_wallets}{Style.RESET_ALL} wallets"
+                f" for {Fore.GREEN + Style.BRIGHT}{menu[input_chain - 1].split()[1]} Network{Style.RESET_ALL}.\n")
+        
+        return input_chain, num_wallets, result_print
+
+    generator = eth_wallet_generator()
+    input_chain, num_wallets, result_print = main_menu()
+    print_banner()
+    print(result_print)
+
+    wallets = generator.generate_batch_eth_wallets(num_wallets=num_wallets)
+    generator.export_wallets_to_csv(wallets, filename="80_eth_wallets")
+    generator.export_wallets_to_excel(wallets, filename="80_eth_wallets")
+    print("")
--- utils/eth_wallet.py
+++ utils/eth_wallet.py
@@ -3,7 +3,7 @@ from eth_account import Account
 from mnemonic import Mnemonic
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from colorama import *
+from colorama import Fore, Style
 import os, secrets
 
 
@@ -12,19 +12,15 @@ class eth_wallet_generator:
         self.name_file = "eth_wallet"  # Default filename for exporting wallets
         
     def logger(self, header:str, header_color, message: str, end="\n"):
-        print(f"{header_color}[{header:<7}]{Style.RESET_ALL} | {message}", end=end)
+        print(f"{header_color}[{header.center(8)}]{Style.RESET_ALL} | {message}", end=end)
 
-    def generate_additional_entropy(self):
+
+    def create_eth_wallet(self):
 
         random_bytes1 = secrets.token_bytes(32)
         random_bytes2 = os.urandom(32)
         combined_entropy = random_bytes1 + random_bytes2
 
-        return combined_entropy
-
-    def create_eth_wallet(self):
-
-        combined_entropy = self.generate_additional_entropy()
         mnemo = Mnemonic("english")
         seed = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
@@ -32,16 +28,14 @@ class eth_wallet_generator:
             salt=combined_entropy,  # Entropy gabungan digunakan sebagai salt
             iterations=100000
         ).derive(b"Secure Seed Phrase")  # Teks bisa diganti sesuai kebutuhan
-
         seed_phrase = mnemo.to_mnemonic(seed)
+        
         Account.enable_unaudited_hdwallet_features()
         account = Account.from_mnemonic(seed_phrase)
         private_key = "0x"+account.key.hex()
         address = account.address
 
         return address, private_key, seed_phrase
-
-
 
     def generate_batch_eth_wallets(self, num_wallets:int=10)-> list:
         """
@@ -63,21 +57,18 @@ class eth_wallet_generator:
         for i in range(num_wallets):
             address, private_key, seed_phrase = self.create_eth_wallet()
             wallets.append({
-                "address": address,
-                "private_key": private_key,
-                "seed_phrase_24": seed_phrase
-            })
-            # print("cek")
+                            "address": address,
+                            "private_key": private_key,
+                            "seed_phrase_24": seed_phrase
+                            })
             self.logger(header=f"{i+1}/{num_wallets}", header_color=Fore.BLUE + Style.BRIGHT, 
-                        message=f"Wallet {i+1} created: {address[:6]}***{address[-6:]}", end="\r")
+                        message=f"Wallet {i+1} created: Address {address[:6]}***{address[-6:]}", end="\r")
         print("")
-
         self.logger("Status", header_color=Fore.CYAN + Style.BRIGHT,
                     message=f"Successfully generated {num_wallets} Ethereum wallets.")
         
         return wallets
 
-        
     def export_wallets_to_csv(self, wallets: list, filename: str =None):
 
         if not os.path.exists("results"):
@@ -92,7 +83,7 @@ class eth_wallet_generator:
         df = pd.DataFrame(wallets)
         df.to_csv(filepath, index=False)
         self.logger("Status", header_color=Fore.CYAN + Style.BRIGHT,
-                    message=f"Successfully export wallets {Fore.GREEN + Style.BRIGHT}({filename}.csv){Style.RESET_ALL}, check results folder.")
+                    message=f"Successfully export {len(df)} wallets to {Fore.GREEN + Style.BRIGHT}{filename}.csv{Style.RESET_ALL}, check results folder.")
 
     def export_wallets_to_excel(self, wallets: list, filename: str =None):
 
@@ -108,4 +99,4 @@ class eth_wallet_generator:
         df = pd.DataFrame(wallets)
         df.to_excel(filepath, index=False)
         self.logger("Status", header_color=Fore.CYAN + Style.BRIGHT,
-                    message=f"Successfully export wallets {Fore.GREEN + Style.BRIGHT}({filename}.xlsx){Style.RESET_ALL}, check results folder.")
+                    message=f"Successfully export {len(df)} wallets to {Fore.GREEN + Style.BRIGHT}{filename}.xlsx{Style.RESET_ALL}, check results folder.")
